c69cc611c01082f6576ef4199d92e61645d5c911
Connor Todd <todd37@purdue.edu>
2019-01-26 15:20:53 -0500

    Add comments

--- backend/app.js
+++ backend/app.js
@@ -13,6 +13,7 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+/* Access Headers */
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Authorization, token, Origin, X-Requested-With, Content-Type, Accept");
