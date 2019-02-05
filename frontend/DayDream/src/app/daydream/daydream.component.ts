2bc2fc08f13380402fd3edc1ddf30c77b035b1f9
Ryan Walden <ryanw758@gmail.com>
2019-02-05 16:42:06 -0500

    Added DayDream component

---
+++ frontend/DayDream/src/app/daydream/daydream.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-daydream',
+  templateUrl: './daydream.component.html',
+  styleUrls: ['./daydream.component.scss']
+})
+export class DaydreamComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
