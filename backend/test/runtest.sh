2629594ed90f3c8314c8ba1078dab9e8920b3e8c
Connor Todd <todd37@purdue.edu>
2019-02-07 01:54:27 -0500

    Edit test suite

--- backend/test/runtest.sh
+++ backend/test/runtest.sh
@@ -1,5 +1,20 @@
+clear
+
+# user tests
+echo USER TESTS
+echo ------------------------------------------------------------------------
+
 mocha test/user/test_register.js --exit
 mocha test/user/test_login.js --exit
 mocha test/user/test_change_email.js --exit
 mocha test/user/test_all_circles.js --exit
 mocha test/user/test_forgot_password.js --exit
+
+sleep 2
+
+#submission tests
+clear
+echo CIRCLE TESTS
+echo ------------------------------------------------------------------------
+
+mocha test/circle/test_add_circle.js --exit
