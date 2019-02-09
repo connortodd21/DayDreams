179a854250b95b44f4bcbd0eeee093ca1c0b8830
Connor Todd <todd37@purdue.edu>
2019-02-08 21:52:16 -0500

    Add delete circle unit test, update test suite

---
+++ backend/test/circle/test_remove_circle.js
@@ -0,0 +1,151 @@
+var chai = require('chai');
+var chaiHttp = require('chai-http');
+var server = require('../../app');
+var User = require('../../model/user');
+var Circle = require('../../model/circle')
+var should = require('chai').should();
+
+chai.use(chaiHttp);
+
+
+var uname = process.env.UNIT_TEST_USERNAME
+var pword = process.env.UNIT_TEST_PASSWORD
+var mail = process.env.UNIT_TEST_EMAIL
+
+describe('Test Delete Circle', () => {
+    before((done) => {
+        var info = {
+            username: uname,
+            password: pword,
+            email: mail,
+        }
+        chai.request(server)
+            .post('/user/register')
+            .set('content-type', 'application/x-www-form-urlencoded')
+            .send(info).then(() => {
+                chai.request(server)
+                    .post('/user/login')
+                    .set('content-type', 'application/x-www-form-urlencoded')
+                    .send(info)
+                    .then((res) => {
+
+                        var circleInfo = {
+                            circleName: 'RAnDoM CirCLE NamE'
+                        }
+
+                        chai.request(server)
+                            .post('/circle/add')
+                            .set('content-type', 'application/x-www-form-urlencoded')
+                            .set('token', res.headers.token)
+                            .send(circleInfo)
+                            .then((circRes) => {
+                                done()
+                            })
+                    })
+            })
+    })
+
+    after((done) => {
+        User.deleteOne({ username: uname }).then(() => {
+            Circle.deleteOne({ circleName: 'RAnDoM CirCLE NamE' }).then(() => {
+                done()
+            })
+        })
+    })
+
+    describe('Delete circle without circle id', () => {
+        it('Should return 400', (done) => {
+            User.findOne({ username: uname }).then((user) => {
+                var token = user['tokens'][0]['token'][0]
+                Circle.findOne({ circleName: 'RAnDoM CirCLE NamE' }).then((circ) => {
+                    var info = {
+                        circleID: circ._id.toString()
+                    }
+                    chai.request(server)
+                        .post('/circle/delete')
+                        .set('content-type', 'application/x-www-form-urlencoded')
+                        .set('token', token)
+                        .send()
+                        .end((err, res) => {
+                            res.should.have.status(400)
+                            done()
+                        })
+                })
+            }).catch((err) => {
+
+            })
+        })
+    })
+
+    describe('Add user with invalid circle id', () => {
+        it('Should return 400', (done) => {
+            User.findOne({ username: uname }).then((user) => {
+                var token = user['tokens'][0]['token'][0]
+                Circle.findOne({ circleName: 'RAnDoM CirCLE NamE' }).then((circ) => {
+                    var info = {
+                        circleID: circ._id.toString() + '1'
+                    }
+                    chai.request(server)
+                        .post('/circle/delete')
+                        .set('content-type', 'application/x-www-form-urlencoded')
+                        .set('token', token)
+                        .send(info)
+                        .end((err, res) => {
+                            res.should.have.status(400)
+                            done()
+                        })
+                })
+            }).catch((err) => {
+
+            })
+        })
+    })
+
+    describe('Delete circle with invalid auth', () => {
+        it('Should return 401', (done) => {
+            User.findOne({ username: uname }).then((user) => {
+                var token = user['tokens'][0]['token'][0]
+                Circle.findOne({ circleName: 'RAnDoM CirCLE NamE' }).then((circ) => {
+                    var info = {
+                        circleID: circ._id.toString()
+                    }
+                    chai.request(server)
+                        .post('/circle/delete')
+                        .set('content-type', 'application/x-www-form-urlencoded')
+                        .set('token', 'bad token')
+                        .send(info)
+                        .end((err, res) => {
+                            res.should.have.status(401)
+                            done()
+                        })
+                })
+            }).catch((err) => {
+
+            })
+        })
+    })
+
+    describe('Delete circle with correct info', () => {
+        it('Should return 200', (done) => {
+            User.findOne({ username: uname }).then((user) => {
+                var token = user['tokens'][0]['token'][0]
+                Circle.findOne({ circleName: 'RAnDoM CirCLE NamE' }).then((circ) => {
+                    var info = {
+                        circleID: circ._id.toString()
+                    }
+                    chai.request(server)
+                        .post('/circle/delete')
+                        .set('content-type', 'application/x-www-form-urlencoded')
+                        .set('token', token)
+                        .send(info)
+                        .end((err, res) => {
+                            res.should.have.status(200)
+                            done()
+                        })
+                })
+            }).catch((err) => {
+
+            })
+        })
+    })
+})
