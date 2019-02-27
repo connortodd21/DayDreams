edc9a87b84cb0d12850084c60e536167a9897921
kcsodetz <kensodetz@gmail.com>
2019-02-27 08:24:28 -0500

    reformat signup.component.ts

--- frontend/DayDream/src/app/auth/signup/signup.component.ts
+++ frontend/DayDream/src/app/auth/signup/signup.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild, ElementRef} from "@angular/core";
+import { Component, OnInit, ViewChild, ElementRef } from "@angular/core";
 import { NgForm, FormGroup, FormBuilder, Validators } from "@angular/forms";
 import { AuthService } from "../../services/auth.service";
 
@@ -17,38 +17,38 @@ export class SignUpComponent implements OnInit {
 
     constructor(public authService: AuthService, private formBuilder: FormBuilder) { }
 
-    ngOnInit(){
+    ngOnInit() {
         this.signUpForm = this.formBuilder.group({
             username: ['', Validators.required],
             password: ['', [Validators.required, Validators.minLength(8)]],
             confirmPassword: ['', Validators.required],
             email: ['', [Validators.required, Validators.email]],
-        }, {validator: this.checkPasswords });
+        }, { validator: this.checkPasswords });
     }
 
     private checkPasswords(group: FormGroup) { // here we have the 'passwords' group
-    let pass = group.controls.password.value;
-    let confirmPass = group.controls.confirmPassword.value;
+        let pass = group.controls.password.value;
+        let confirmPass = group.controls.confirmPassword.value;
 
-    return pass === confirmPass ? null : { notSame: true }
-  }
+        return pass === confirmPass ? null : { notSame: true }
+    }
 
-  get form() { return this.signUpForm.controls }
+    get form() { return this.signUpForm.controls }
 
     get response_msg() { return this.response; }
 
-    closeAlert(){
+    closeAlert() {
         this.alert.nativeElement.classList.remove('show');
     }
 
-  signUp(form: NgForm){
-    this.submitted = true;
+    signUp(form: NgForm) {
+        this.submitted = true;
         if (this.signUpForm.invalid) {
-            console.log(this.signUpForm)
+            console.log(this.signUpForm);
             return;
         }
         this.authService.registerUser(form.value.email, form.value.username, form.value.password).then((res) => {
-            this.response = "complete"
+            this.response = "complete";
         }).catch((err) => {
             if (err.error.message == "User already exists") {
                 this.response = "duplicate";
@@ -57,5 +57,5 @@ export class SignUpComponent implements OnInit {
                 this.response = "fatalError";
             }
         })
-  }
+    }
 }
