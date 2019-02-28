36417d0c6b843920bb543421760c93ecc746f8e5
kcsodetz <kensodetz@gmail.com>
2019-02-27 22:00:49 -0500

    Add validation for create circle (Fixes #25)

--- frontend/DayDream/src/app/home/home.component.ts
+++ frontend/DayDream/src/app/home/home.component.ts
@@ -16,58 +16,65 @@ export class HomeComponent implements OnInit {
   myCircles: Circle[];
   fileForm: FormGroup;
   image: string;
+  submitted = false;
 
   constructor(private userService: UserService, private circleService: CircleService, private formBuilder: FormBuilder, private _router: Router) {
-    this.fileForm = this.formBuilder.group({
-      imageUrl: ['', Validators.required],
-      image_id: ['', Validators.required],
-      circleDesc: ['', Validators.required],
-      circleName: ['', Validators.required],
-    });
-    
+    ;
+
   }
 
   circleName: '';
   circleDesc: '';
-  
+
 
   ngOnInit() {
     this.displayCircles();
+
+    this.fileForm = this.formBuilder.group({
+      imageUrl: [''],
+      circleDesc: ['', Validators.required],
+      circleName: ['', Validators.required],
+    })
   }
 
- 
+get form() { return this.fileForm.controls; }
 
-  renderCircle(circle:Circle){
-      /* Navigate to /circle/id  */
-      this._router.navigate(['/circle/' + circle.ID]);
+  renderCircle(circle: Circle) {
+    /* Navigate to /circle/id  */
+    this._router.navigate(['/circle/' + circle.ID]);
   }
 
   displayCircles() {
     this.userService.getUserCircles().then((data) => {
-      
-      let i:number;
+
+      let i: number;
 
       let response = [];
       response.push(data);
 
       this.myCircles = new Array(response[0].length)
 
-      for(i = 0; i< response[0].length; i+=1) {
-          let circle = new Circle(response[0][i])
-          if(circle.circleName.length > 18){
-            circle.circleName = circle.circleName.substring(0,20) + '...'
-          }
-          this.myCircles[i] = circle;
-       }
+      for (i = 0; i < response[0].length; i += 1) {
+        let circle = new Circle(response[0][i])
+        if (circle.circleName.length > 18) {
+          circle.circleName = circle.circleName.substring(0, 20) + '...'
+        }
+        this.myCircles[i] = circle;
+      }
 
-       console.log(this.myCircles)
+      console.log(this.myCircles)
 
-       console.log(response);
+      console.log(response);
     })
   }
 
   submitFile(event) {
-    console.log(event.value)
+    this.submitted = true;
+    if (this.fileForm.invalid) {
+      console.log(event);
+      return;
+    }
+
     this.circleService.createCircle(event.value.circleName, event.value.circleDesc, event.value.imageUrl).then(() => {
       var confirm = window.alert('Circle ' + event.value.circleName + ' Created!')
       window.location.replace("/home")
