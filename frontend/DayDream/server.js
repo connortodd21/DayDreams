7cf32b6eaffd36df8309c1465ca0ec3aafb9f0d4
Connor Todd <todd37@purdue.edu>
2019-03-22 20:28:24 -0400

    add server

---
+++ frontend/DayDream/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Serve only the static files form the dist directory
+app.use(express.static('./dist/critika'));
+
+app.get('/*', function(req,res) {
+    
+res.sendFile(path.join(__dirname,'/dist/DayDream/index.html'));
+});
+
+app.listen(process.env.PORT || 8080)
