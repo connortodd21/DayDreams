681436125658f067f96d8a6c1dcb65a8c3d87db0
Connor Todd <todd37@purdue.edu>
2019-03-22 21:12:07 -0400

    fix server

--- frontend/DayDream/server.js
+++ frontend/DayDream/server.js
@@ -4,7 +4,7 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/critika'));
+app.use(express.static('./dist/DayDream'));
 
 app.get('/*', function(req,res) {
     
